184b37d7e7aa52d0b02bda601e313fe3cf6f9c32
Mansi <devpcctv@gmail.com>
2026-03-29 22:47:53 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    server: {
+        allowedHosts: ['mansi-chatapp-1.onrender.com']
+    }
 });
